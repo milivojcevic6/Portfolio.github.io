6e82fb3970b8f72ec04b0dbb99a373aed17e3bc5
Milan <998miki@gmail.com>
2024-02-18 20:42:39 +0100

    project section updated v4.2 grid

--- projects-render.js
+++ projects-render.js
@@ -6,14 +6,16 @@ const projects = [
         "title": "FixIt",
         "tags": ["PHP"],
         "image": "images/logo.png",
-        "description": "Information system built on the occasion of the Systems III course. The aim is to bring together car lovers from all over the world in one place! It is designed as a kind of blog where users have the opportunity to post their opinions, experiences and offers to help other car lovers.",
+        "description": "Information system built on the occasion of the Systems III course. The aim is to bring together car lovers from all over the world in one place! It is designed as a kind of blog where users have the opportunity to post their opinions, experiences and offers to help other car lovers."+
+        "Information system built on the occasion of the Systems III course. The aim is to bring together car lovers from all over the world in one place! It is designed as a kind of blog where users have the opportunity to post their opinions, experiences and offers to help other car lovers.",
         "link": "https://github.com/milivojcevic6/FixIt"
     },
     {
         "title": "AR Tunes",
         "tags": ["Unity"],
         "image": "images/ar.png",
-        "description": "Listening to music on an augmented vinyl player or radio, accompanied with objects reacting to the beat. In collaboration with Karolina Trajkovska.",
+        "description": "Information system built on the occasion of the Systems III course. The aim is to bring together car lovers from all over the world in one place! It is designed as a kind of blog where users have the opportunity to post their opinions, experiences and offers to help other car lovers."+
+            "Information system built on the occasion of the Systems III course. The aim is to bring together car lovers from all over the world in one place! It is designed as a kind of blog where users have the opportunity to post their opinions, experiences and offers to help other car lovers.",
         "link": "https://gitlab.com/karlyy/vuforiamuseum"
     },
     {
@@ -54,7 +56,7 @@ function generateProjectHTML(project) {
     return `
     <div class="project shadow-large ${project.tags.join(' ')}" data-tags="${project.tags.join(' ')}">
         <div class="project-image">
-            <img src="${project.image}" width="300" height="300" alt="${project.title}"/>
+            <img src="${project.image}" alt="${project.title}"/>
         </div>
         <div class="project-info">
             <h3>${project.title}</h3>
